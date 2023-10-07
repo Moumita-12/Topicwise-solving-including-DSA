996e9ba22d25b29da5ee2ce62da818417433807d
Moumita Shib <120558169+Moumita-12@users.noreply.github.com>
2023-10-07 12:01:02 +0600

    Create codechef 7 Rings.cpp

---
+++ Basic math/codechef 7 Rings.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    
+	    int a,b;
+	    cin>>a>>b;
+	   
+	    int k = a*b;
+	    
+	    if(k>=10000 && k<=99999)   cout<<"yes"<<endl;
+	    else   cout<<"no"<<endl;
+	    
+	    
+	    
+	}
+
+	return 0;
+}
